e92e92d4c2bcdb70b7ba910f3b8d3a904f65d34e
Will Santos <wlds.will.1@gmail.com>
2022-10-05 18:48:09 -0300

    Update script.js

--- project.js/script.js
+++ project.js/script.js
@@ -4,12 +4,14 @@ var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
-if (hora >= 0 && hora <12) { 
-    img.src='manha.png'
+if (hora >= 0 && hora < 12) { 
+    img.src = 'imagens/manha.png'
+
     } else if (hora >= 12 && hora < 18) {  
-        img.src = 'tarde.png'
+        img.src = 'imagens/tarde.png'
+
     } else {
-        img.src = 'noite.png'
+        img.src = 'imagens/noite.png'
     }
 
 }
