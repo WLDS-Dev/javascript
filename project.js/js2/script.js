b5e094429b524d0877cf002ca63cb582119cad91
Will Santos <wlds.will.1@gmail.com>
2022-10-07 16:21:10 -0300

    manutenção

---
+++ project.js/js2/script.js
@@ -0,0 +1,21 @@
+function carregar () {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours() 
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) { 
+    img.src = 'imagens/manha.png'
+    document.body.style.background = '#a0c8c8'
+
+    } else if (hora >= 12 && hora < 18) {  
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#bd7443'
+
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#256a86'
+    }
+
+}
+
