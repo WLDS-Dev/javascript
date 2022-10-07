744be2bada87ebe698345eb70d4065aadc7a2ebf
Will Santos <wlds.will.1@gmail.com>
2022-10-07 18:38:08 -0300

    2

--- project.js/js2/script.js
+++ project.js/js2/script.js
@@ -9,14 +9,42 @@ function verificar () {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
-        if (fsex[0].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+            if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 12) { 
+                img.setAttribute('src', 'imagens/menino.png')
+            } else if (idade < 22) {  
+                img.setAttribute('src', 'imagens/menino2.png')
+            } else if (idade < 42) { 
+                img.setAttribute('src', 'imagens/homem.png')             
+            } else if (idade < 57) {  
+                img.setAttribute('src', 'imagens/homem2.png')     
+            } else {
+                img.setAttribute('src', 'imagens/velho.png')
+
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade < 12) {  
+                img.setAttribute('src', 'imagens/menina.png') 
+            } else if (idade < 22) {   
+                img.setAttribute('src', 'imagens/menina2.png')             
+            } else if (idade < 42) {   
+                img.setAttribute('src', 'imagens/mulher.png')    
+            } else if (idade < 57) {
+                img.setAttribute('src', 'imagens/mulher2.png')
+            } else {
+                img.setAttribute('src', 'imagens/velha.png')
+
+            }
         }
+
+        res.style.textAlign = 'center'
         res.innerHTML = `${gênero} com ${idade} ano(s).`
-        
-        
+        res.appendChild(img)
+          
     }
 
 }
